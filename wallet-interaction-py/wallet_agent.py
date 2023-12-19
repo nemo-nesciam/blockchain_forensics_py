6079618c512ff534adf68c848f81af28eb40141c
noesis <noesis5@pm.me>
2023-12-19 16:56:25 -0500

    added bytecode_reverser_agent

--- wallet-interaction-py/wallet_agent.py
+++ wallet-interaction-py/wallet_agent.py
@@ -23,7 +23,7 @@ else:
 print(f'{Fore.WHITE}---------------------------------------------------------')
 
 # Connect to a target smart contract
-target_address = web3.to_checksum_address(TARGET_ADDRESS_1)
+target_address = web3.to_checksum_address(CHAINLINK_TOKEN_CONTRACT)
 print(f'{Fore.WHITE}Type of address: {Fore.MAGENTA}{(web3.eth.get_code(target_address))}')
 print(f'{Fore.WHITE}Total balance: {Fore.MAGENTA}{web3.from_wei(web3.eth.get_balance(target_address), "ether")}')
 
